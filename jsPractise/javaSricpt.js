8853dd60e7a03f6f387539283bf81ecd8600f2ab
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-09-26 18:26:42 +0530

    third commit

--- jsPractise/javaSricpt.js
+++ jsPractise/javaSricpt.js
@@ -4,8 +4,18 @@ let strins=`sanjeet`;
 let str = string.toLowerCase();
 let str2=strins.toUpperCase();
 
-let str3=string.indexOf();
+let str3=string.indexOf(`JEET`);
 
 console.log(str);
 console.log(str2);
 console.log(str3);
+
+
+
+let msg = "   hello   ";
+
+console.log(msg.trim().toUpperCase());
+
+let name= "apnaCollege";
+
+console.log(name.slice(4).replace("l","t"));
