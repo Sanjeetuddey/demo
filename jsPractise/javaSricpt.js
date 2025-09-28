32f622ccf8d74dfcdb3ae01e8ade14f32b7246b8
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-09-28 21:45:52 +0530

    day4

--- jsPractise/javaSricpt.js
+++ jsPractise/javaSricpt.js
@@ -1,20 +1,35 @@
-let arr = ["january","july","march","august"];
+// let arr = ["january","july","march","august"];
 
-console.log(arr.splice(0,1));
-console.log(arr.splice(1,0,"june"));
+// console.log(arr.splice(0,1));
+// console.log(arr.splice(1,0,"june"));
  
-console.log(arr);
+// console.log(arr);
  
 
-let arr1 = ["c","c++","html","javascript","python","java","c#","sql"];
+// let arr1 = ["c","c++","html","javascript","python","java","c#","sql"];
 
-let reverse = arr1.reverse();
+// let reverse = arr1.reverse();
 
-console.log(reverse.indexOf("javascript"));
+// console.log(reverse.indexOf("javascript"));
 
-let tictac=[["X","","O"],["","X",""],["O","","X"]];
+// let tictac=[["X","","O"],["","X",""],["O","","X"]];
 
-console.log(tictac);
+// console.log(tictac);
+
+let arr = [7,0,6,-2];
+
+let n=3;
+
+let ans=arr.slice(-n);
+
+console.log(ans);
+
+
+
+
+
+
+
 
 
 
