c87d36a159eee0b10a2d9b9739ebcb914f6a96b4
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-09-30 22:37:20 +0530

    commit

--- jsPractise/javaSricpt.js
+++ jsPractise/javaSricpt.js
@@ -1,69 +1,10 @@
-// let arr = ["january","july","march","august"];
+const fav="Suit";
 
-// console.log(arr.splice(0,1));
-// console.log(arr.splice(1,0,"june"));
- 
-// console.log(arr);
- 
+let ans=prompt("your favrite movie");
 
-// let arr1 = ["c","c++","html","javascript","python","java","c#","sql"];
-
-// let reverse = arr1.reverse();
-
-// console.log(reverse.indexOf("javascript"));
-
-// let tictac=[["X","","O"],["","X",""],["O","","X"]];
-
-// console.log(tictac);
-
-// let arr = [7,0,6,-2];
-
-// let n=3;
-
-// let ans=arr.slice(-n);
-
-// console.log(ans);
-
-//ytutedtgutfyrfugyt
-
-//yffjtctjyvyjvuy
-
-// for (let i = 1; i <= 15; i++) {
-  
-//     if (i%2==!0) 
-//         {
-//         console.log(i);
-//          }
-// }
-
-
-
-// for (let i = 1; i <= 10; i++) {
-  
-//     if (i%2==0) 
-//         {
-//         console.log(i);
-//          }
-// }
-
-
-// for (let i = 5; i <= 50; i++) {
-  
-//     if (i%5==0) 
-//         {
-//         console.log(i);
-//          }
-// }
-
-
-let fav=`Suit`;
-
-let ans=prompt(`your favrite movie`);
-
-while ((fav=!ans)&&(ans!=`quit`)) 
+while ((ans!=fav)&&(ans!="quit")) 
     {
-        console.log(`wron`);
-        ans=prompt(`plase enter try`);
+        ans=prompt("plase enter try");
         
 }
 
