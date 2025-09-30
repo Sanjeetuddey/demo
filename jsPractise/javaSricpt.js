888dc4704f73e7377c04886079ba5b3c538a5abe
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-09-30 21:30:06 +0530

    day6

--- jsPractise/javaSricpt.js
+++ jsPractise/javaSricpt.js
@@ -16,19 +16,58 @@
 
 // console.log(tictac);
 
-let arr = [7,0,6,-2];
+// let arr = [7,0,6,-2];
 
-let n=3;
+// let n=3;
 
-let ans=arr.slice(-n);
+// let ans=arr.slice(-n);
 
-console.log(ans);
+// console.log(ans);
 
 //ytutedtgutfyrfugyt
 
 //yffjtctjyvyjvuy
 
+// for (let i = 1; i <= 15; i++) {
+  
+//     if (i%2==!0) 
+//         {
+//         console.log(i);
+//          }
+// }
 
+
+
+// for (let i = 1; i <= 10; i++) {
+  
+//     if (i%2==0) 
+//         {
+//         console.log(i);
+//          }
+// }
+
+
+// for (let i = 5; i <= 50; i++) {
+  
+//     if (i%5==0) 
+//         {
+//         console.log(i);
+//          }
+// }
+
+
+let fav=`Suit`;
+
+let ans=prompt(`your favrite movie`);
+
+while ((fav=!ans)&&(ans!=`quit`)) 
+    {
+        console.log(`wron`);
+        ans=prompt(`plase enter try`);
+        
+}
+
+ 
 
 
 
