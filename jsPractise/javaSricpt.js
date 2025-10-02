596f33c13adf7fc5efd07983fc8102edff1159c7
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-10-02 20:02:04 +0530

    day7

--- jsPractise/javaSricpt.js
+++ jsPractise/javaSricpt.js
@@ -1,14 +1,14 @@
-const fav="Suit";
-
-let ans=prompt("your favrite movie");
-
-while ((ans!=fav)&&(ans!="quit")) 
-    {
-        ans=prompt("plase enter try");
-        
+let arr = 12345623;
+let count=0;
+let copy=arr;
+while (copy>0)
+ {
+    count++;
+     
+    console.log(copy);
 }
+console.log(count);
 
- 
 
 
 
