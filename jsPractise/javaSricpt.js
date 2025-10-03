f0b152084a3d6d07855c2343ef118e4a5aa9407f
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-10-04 00:15:57 +0530

    day8

--- jsPractise/javaSricpt.js
+++ jsPractise/javaSricpt.js
@@ -1,30 +1,35 @@
-// let arr = 12345623;
-// let count=0;
-// let copy=arr;
-// while (copy>0)
-//  {
-//     count++;
-     
-//     console.log(copy);
-// }
-// console.log(count);
+let guess = prompt("please enter the limit");
+
+let num = Math.floor(Math.random() * guess)+1;
+console.log(num);
+
+
+let guess1 = prompt("please enter the number of u guess");
+
+while (true) 
+    {
+        if (num==guess1) 
+         {
+                console.log(`congratulation your guess is correst ${num} `);
+                break;    
+        }
+        if (guess1=="quit") 
+        {
+            console.log("you quit the game");
+            break;
+        }
+            else
+                {
+                guess1=prompt("wrong guess please enter again");
+            }
+};
 
 
 
-// let random22 = Math.random();
-// console.log(random22);
 
-// let random1=random22*10;
-// console.log(random1);
 
-// let random2=Math.floor(random1);
-// console.log(random2);
 
-// let random3=random2 + 1;
-// console.log(random3);
 
-let ans = Math.floor(Math.random()*5)+1;
-console.log(ans);
 
 
 
