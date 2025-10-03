bfae8f8aa1c296dc4121296e0743ccf4cfcb425d
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-10-03 23:25:39 +0530

    last commit

--- jsPractise/javaSricpt.js
+++ jsPractise/javaSricpt.js
@@ -1,19 +1,30 @@
-let arr = 12345623;
-let count=0;
-let copy=arr;
-while (copy>0)
- {
-    count++;
+// let arr = 12345623;
+// let count=0;
+// let copy=arr;
+// while (copy>0)
+//  {
+//     count++;
      
-    console.log(copy);
-}
-console.log(count);
+//     console.log(copy);
+// }
+// console.log(count);
 
 
 
+// let random22 = Math.random();
+// console.log(random22);
 
+// let random1=random22*10;
+// console.log(random1);
 
+// let random2=Math.floor(random1);
+// console.log(random2);
 
+// let random3=random2 + 1;
+// console.log(random3);
+
+let ans = Math.floor(Math.random()*5)+1;
+console.log(ans);
 
 
 
