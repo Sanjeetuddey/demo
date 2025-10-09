cbe239301eb6eb8761c8af178c1d4838c15d59c5
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-10-09 20:55:45 +0530

    day12

--- jsPractise/javaSricpt.js
+++ jsPractise/javaSricpt.js
@@ -1,8 +1,3 @@
-// Qs3. Write a JavaScript function that accepts a list of country names as input and
-// returns the longest country name as output.
-// Example : country = ["Australia", "Germany", "United States of America"] output :
-// "United States of America"
-
 
 let str= ["Australia", "Germany", "United States of America"];
 
@@ -21,7 +16,6 @@ function longName(str)
     }
     return str[ansIndex];
 }
-//rrdyttdfyfytftftyft
 
 console.log(longName(str));
 
