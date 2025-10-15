a60570b63300cea01522c3e9482ee1a981bbadd8
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-10-15 15:44:42 +0530

    day13

--- jsPractise/javaSricpt.js
+++ jsPractise/javaSricpt.js
@@ -1,25 +1,10 @@
-
-let str= ["Australia", "Germany", "United States of America"];
-
-
-function longName(str)
-{
-    let ansIndex=0;
-    for (let i=0; i<str.length; i++) {
-
-        let curr=str[ansIndex].length;
-        let len=str[i].length;
-        if (len>curr) 
-            {
-           ansIndex=i;
-        }
-    }
-    return str[ansIndex];
+const nitin = {
+name:"nitin",
+age:22,
+role:"developer",
+experienced:"5 years",
 }
 
-console.log(longName(str));
 
 
-
-
-
+console.log(nitin);
